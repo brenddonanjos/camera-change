be1838f934e27596abe7c680a2a58c36d1f0da0e
brenddonanjos <brenddon.dev@gmail.com>
2024-12-09 09:28:26 -0300

    init.sh

--- init.sh
+++ init.sh
@@ -10,5 +10,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce -y
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&\
 sudo chmod +x /usr/local/bin/docker-compose
